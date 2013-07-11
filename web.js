cd04b07475ed860149bad3a0aefe2f3d92317851
Heleen Maibak <laanekass@gmail.com>
2013-07-11 11:38:12 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs =require('fs');
-  var buffer = new Buffer(fs.readFileSync('index.html'));
-  response.send(buffer.toString('utf-8');
+
+  response.send(fs.readFileSync('index.html').toString();
 });
 
 var port = process.env.PORT || 5000;
